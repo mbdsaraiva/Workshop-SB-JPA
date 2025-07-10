5ce053b882e81a22ba476eded3ad8dcd3e3dc546
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-07-09 21:44:14 -0300

    Category

--- src/main/java/com/mbds/course/config/TestConfig.java
+++ src/main/java/com/mbds/course/config/TestConfig.java
@@ -1,6 +1,4 @@
 package com.mbds.course.config;
-
-import java.lang.reflect.Array;
 import java.time.Instant;
 import java.util.Arrays;
 
@@ -9,9 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.mbds.course.entities.Category;
 import com.mbds.course.entities.Order;
 import com.mbds.course.entities.User;
 import com.mbds.course.entities.enums.OrderStatus;
+import com.mbds.course.repositories.CategoryRepository;
 import com.mbds.course.repositories.OrderRepository;
 import com.mbds.course.repositories.UserRepository;
 
@@ -26,8 +26,17 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Eletronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
